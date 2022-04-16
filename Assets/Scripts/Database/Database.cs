5e8a34af1137b2811e56438ff0e97256691179e9
MohammadAli Saeidi <1saeidima@gmail.com>
2022-04-17 01:36:38 +0430

    Update Database.cs

--- Assets/Scripts/Database/Database.cs
+++ Assets/Scripts/Database/Database.cs
@@ -1,12 +1,23 @@
+using SQLite4Unity3d;
 using UnityEngine;
 
 namespace BallBalance.Database
 {
     public static class Database
-    {
-        internal static void Init()
-        {
+	{
+		internal static string FileRootPath { get; private set; }
+		private static SQLiteConnection _connection;
 
-        }
+		internal static void Init()
+		{
+			//set constant addresses
+			FileRootPath = Application.persistentDataPath + "/";
+
+			//create SQLite Connection
+			_connection = new SQLiteConnection(FileRootPath + "ball_balance.db", SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+
+			//create tables
+			_connection.CreateTable<Account>();
+		}
     }
 }

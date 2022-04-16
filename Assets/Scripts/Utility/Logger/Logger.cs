ab958208b13cc776615226e7b782e6c02ada5760
MohammadAli Saeidi <1saeidima@gmail.com>
2022-04-17 01:49:37 +0430

    Update Logger.cs

--- Assets/Scripts/Utility/Logger/Logger.cs
+++ Assets/Scripts/Utility/Logger/Logger.cs
@@ -1,9 +1,46 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace BallBalance.Utility.Logger
 {
-    public class Logger : MonoBehaviour
-    {
+	public class Logger : MonoBehaviour
+	{
+		// TODO: if the build is on developer mode. then this script will work
+		[SerializeField] Button FloatingButton;
+		[SerializeField] GameObject Panel;
+		[SerializeField] Text LogText;
 
-    }
+		bool isShowing = false;
+
+		void Awake()
+		{
+			// Init floating button
+			FloatingButton.onClick.AddListener(delegate
+			{
+				if (isShowing)
+				{
+					isShowing = false;
+				}
+				else
+				{
+					isShowing = true;
+				}
+			});
+		}
+
+		internal void Log(string text)
+		{
+			LogText.text = $"{LogText.text}\n{text}";
+		}
+
+		internal void ShowLogs()
+		{
+			Panel.SetActive(true);
+		}
+
+		internal void HideLogs()
+		{
+			Panel.SetActive(false);
+		}
+	}
 }

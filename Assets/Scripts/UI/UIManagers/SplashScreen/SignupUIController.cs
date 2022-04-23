d779253fd4b042947921f532e7c732fe5ba3e12b
MohammadAli Saeidi <1saeidima@gmail.com>
2022-04-24 03:34:35 +0430

    Add Fonts
    Create SignupUIController.cs

---
+++ Assets/Scripts/UI/UIManagers/SplashScreen/SignupUIController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BallBalance.UISystem;
+
+
+namespace BallBalance.Signup
+{
+	public class SignupUIController : UISystem.UISystem
+	{
+		protected override void GetAllScreens()
+		{
+		}
+	}
+}

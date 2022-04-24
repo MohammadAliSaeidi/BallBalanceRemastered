f0bd62431f82bb41a003f3786c8722c173907411
MohammadAli Saeidi <1saeidima@gmail.com>
2022-04-25 03:21:57 +0430

    Create EventHandler.cs

---
+++ Assets/Scripts/Utility/EventHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace BallBalance
+{
+	public static class EventHandler
+	{
+		public static void FireEvent(ref UnityEvent unityEvent)
+		{
+			if (unityEvent != null)
+			{
+				unityEvent.Invoke();
+			}
+		}
+
+		public static void FireEvent<T>(ref UnityEvent<T> unityEvent, T argument)
+		{
+			if (unityEvent != null)
+			{
+				unityEvent.Invoke(argument);
+			}
+		}
+	}
+}

4407aa46fd538eb743c55c38ed123b98e0672c76
MohammadAli Saeidi <1saeidima@gmail.com>
2022-04-24 05:55:17 +0430

    Create GameManagerUIController.cs

---
+++ Assets/Scripts/UI/UIManagers/GameManagerUIController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using BallBalance.UISystem;
+
+namespace BallBalance
+{
+	internal class GameManagerUIController : UISystem.UISystem
+	{
+		[SerializeField] private UIScreen s_LoadingEffect;
+
+		private void Awake()
+		{
+			s_LoadingEffect.DelayBeforeClosingScreen = 1;
+		}
+
+		protected override void GetAllScreens()
+		{
+			Screens.Add(s_LoadingEffect);
+		}
+
+		internal void ShowLoadingEffect()
+		{
+			s_LoadingEffect.Show();
+		}
+
+		internal void HideLoadingEffect()
+		{
+			s_LoadingEffect.Close();
+		}
+	}
+}

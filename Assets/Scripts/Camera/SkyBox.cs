5d0cbffd37cf2c26c4190b775310f5824ab630f5
MohammadAli Saeidi <1saeidima@gmail.com>
2022-06-08 15:58:59 +0430

    Create level_01.unity
    Create Player.prefab
    Add level_01.fbx
    Add SkyBox.cs

---
+++ Assets/Scripts/Camera/SkyBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBox : MonoBehaviour
+{
+    public float colorChangeSpeed = 0.36f;
+    private float h;
+    public float s;
+    public float v;
+
+
+    private void Update()
+    {
+        h += colorChangeSpeed * Time.deltaTime;
+        if (h >= 1)
+            h = 0;
+        Camera.main.backgroundColor = Color.HSVToRGB(h, s, v);
+
+        Mathf.Clamp(h, 0, 1);
+        Mathf.Clamp(s, 0, 1);
+        Mathf.Clamp(v, 0, 1);
+    }
+}
